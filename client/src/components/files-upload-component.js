6d97d5d3b4c8723e9e6af26da92530baa6a28bf7
Travis Kerr <kerrta@users.noreply.github.com>
2020-02-01 17:38:17 -0500

    Initial client UI

---
+++ client/src/components/files-upload-component.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class FilesUploadComponent extends Component 
+{
+    constructor(props) 
+    {
+        super(props);
+
+        this.onFileChange = this.onFileChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = 
+        {
+            imgCollection: ''
+        }
+    }
+
+    onFileChange(e) 
+    {
+        this.setState({ imgCollection: e.target.files })
+    }
+
+    onSubmit(e) 
+    {
+        e.preventDefault()
+
+        var formData = new FormData();
+        for (const key of Object.keys(this.state.imgCollection)) 
+        {
+            formData.append('imgCollection', this.state.imgCollection[key])
+        }
+        axios.post("http://localhost:4000/api/upload-images", formData, 
+        {
+        }).then(res => {
+            console.log(res.data)
+        })
+    }
+
+    render() 
+    {
+        return (
+                    <form onSubmit={this.onSubmit}>
+                        <div class="custom-file custom-padding-top custom-margin-top">
+                            <input type="file" class="custom-file-input form-control-sm" id="customFile" name="imgCollection" onChange={this.onFileChange} multiple />
+                            <label class="custom-file-label col-form-label-sm" for="customFile">Choose file</label>
+                        </div>
+                        <div className="form-group">
+                            <button class="custom-file-input-width" class="background-1 btn btn-primary " type="submit">Upload</button>
+                        </div>
+                    </form>
+        )
+    }
+}
---
+++ client/src/components/upload.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Upload extends Component 
+{
+  constructor(props) 
+  {
+    super(props)
+  }
+
+  render() 
+  {
+    return <p>Upload</p>
+  }
+}
