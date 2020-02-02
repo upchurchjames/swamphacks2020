54cb40031171edf91391e93c496ca8ff32bb4189
Travis Kerr <kerrta@users.noreply.github.com>
2020-02-02 05:31:48 -0500

    Added results component

--- client/src/components/files-upload-component.js
+++ client/src/components/files-upload-component.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import Results from './results';
 
 export default class FilesUploadComponent extends Component 
 {
+
     constructor(props) 
     {
         super(props);
@@ -14,6 +17,8 @@ export default class FilesUploadComponent extends Component
         {
             imgCollection: ''
         }
+
+        this.setState({showResults: false});
     }
 
     onFileChange(e) 
@@ -35,20 +40,32 @@ export default class FilesUploadComponent extends Component
         }).then(res => {
             console.log(res.data)
         })
+
+        this.setState({showResults: true});
     }
 
     render() 
     {
         return (
-                    <form onSubmit={this.onSubmit}>
-                        <div class="custom-file custom-padding-top custom-margin-top">
-                            <input type="file" class="custom-file-input form-control-sm" id="customFile" name="imgCollection" onChange={this.onFileChange} multiple />
-                            <label class="custom-file-label col-form-label-sm col-md-8 offset-md-2" for="customFile">Upload a picture of ingredients!</label>
-                        </div>
-                        <div className="form-group">
-                            <button class="btn btn-primary" type="submit">Get Recipes</button>
-                        </div>
-                    </form>
+                <div>
+                    <div class="background-2 shadow p-3 mb-5 rounded">
+                        <h1 class="title-text">Flexipe</h1>
+                        <h2>The flexible recipe generator!</h2>
+                    
+                        <form onSubmit={this.onSubmit}>
+                            <div class="custom-file custom-padding-top custom-margin-top">
+                                <input type="file" class="custom-file-input form-control-sm" id="customFile" name="imgCollection" onChange={this.onFileChange} multiple />
+                                <label class="custom-file-label col-form-label-sm col-md-8 offset-md-2" for="customFile">Upload a picture of ingredients!</label>
+                            </div>
+                            <div className="form-group">
+                                <button class="btn btn-primary" type="submit">Get Recipes</button>
+                            </div>
+                        </form>
+                    </div>
+                    
+                    {this.state.showResults ? <Results /> : null}
+                    
+                </div>
         )
     }
 }
---
+++ client/src/components/results.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Results extends Component 
+{
+  constructor(props) 
+  {
+    super(props)
+  }
+
+  render() 
+  {
+    return (
+            <div id="results" class="background-2 custom-margin-top shadow p-3 mb-5 rounded">
+                Some Results
+            </div>
+        )
+  }
+}
