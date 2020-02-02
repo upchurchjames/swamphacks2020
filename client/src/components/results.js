e599de581926ba979d3b4e1633f2803ffd8cc0ff
Travis Kerr <kerrta@users.noreply.github.com>
2020-02-02 06:55:05 -0500

    Name change

--- client/src/components/results.js
+++ client/src/components/results.js
@@ -11,7 +11,13 @@ export default class Results extends Component
   {
     return (
             <div id="results" class="background-2 custom-margin-top shadow p-3 mb-5 rounded">
-                Some Results
+                <ul class="list-group">
+				  <li class="list-group-item">Cras justo odio</li>
+				  <li class="list-group-item">Dapibus ac facilisis in</li>
+				  <li class="list-group-item">Morbi leo risus</li>
+				  <li class="list-group-item">Porta ac consectetur ac</li>
+				  <li class="list-group-item">Vestibulum at eros</li>
+				</ul>
             </div>
         )
   }
